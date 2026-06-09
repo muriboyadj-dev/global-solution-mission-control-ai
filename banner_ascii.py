ca59a288ebd013666da56d648941fcfa96274a00
muriboyadj-dev <muriboyadj@gmail.com>
2026-06-09 18:48:48 -0300

    Create banner_ascii.py

---
+++ banner_ascii.py
@@ -0,0 +1,9 @@
+def exibir_banner():
+    print("""
+============================================================
+              MISSION CONTROL AI - ENVIROSAT
+============================================================
+      Monitoramento Inteligente de Missões Espaciais
+                 Global Solution - FIAP
+============================================================
+""")
